5bc7cf5056c9e746fe92637b857071139db44d42
ipekorkmaz <2309011084@stu.istinye.edu.tr>
2026-01-04 21:12:49 +0300

    ipek korkmaz

--- web_visualization.py
+++ web_visualization.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import plotly.express as px
 
+#Load cleaned air quality dataset
 df=pd.read_csv("data/istanbul_air_quality_clean.csv")
+#Concert date column from string to datetime format for time-based analysis
 df["date"]=pd.to_datetime(df["date"])
 print("Dataset is loaded ready for web visualization.")
 print(df.head())
 
+# This creates a web-based line chart that allows comparison of PM2.5 and PM10 values on the same graph.
 fig=px.line(
     df,
     x="date",
