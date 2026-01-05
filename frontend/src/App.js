807cc78e24f3c7506977862039c9c21f570d1064
ipekorkmaz <2309011084@stu.istinye.edu.tr>
2026-01-05 15:21:07 +0300

    ipek korkmaz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,14 @@ function App() {
   return (
     <div>
       <h1>Air Quality Visualization</h1>
-      <h1>PM2.5 & PM10 Levels by Date in Istanbul (2024)</h1>
+      <h2>PM2.5 & PM10 Levels by Date in Istanbul (2024)</h2>
 
       <Plot
         data={[
           {
+            //This chart creates a web-based visualization for PM2.5 values
+            //The x-axis represents the selected dates
+            //The y-axis represents PM2.5 concentration values
             x: ["2024-01-04", "2024-09-03"],
             y: [30, 115],
             type: "scatter",
@@ -17,6 +20,9 @@ function App() {
             name: "PM2.5",
           },
           {
+            //This chart creates a web-based visualization for PM10 values
+            //The x-axis represents the selected dates
+            //The y-axis represents PM10 concentration values
             x: ["2024-01-04", "2024-09-03"],
             y: [60, 85],
             type: "scatter",
